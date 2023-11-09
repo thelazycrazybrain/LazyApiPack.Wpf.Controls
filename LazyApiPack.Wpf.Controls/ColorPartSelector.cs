ad02d6cade1a5396f099d790f26e249ff7812a45
Jodie Watson <jodie@jodiewatson.net>
2023-11-09 16:16:02 +0100

    Behaviors implemented, Wait circle control implemented (incomplete), Color picker implemented (incomplete)

---
+++ LazyApiPack.Wpf.Controls/ColorPartSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace LazyApiPack.Wpf.Controls
+{
+    public class ColorPartSelector : Slider
+    {
+        public ColorPartSelector()
+        {
+            Minimum = 0;
+            Maximum = 255;
+            SmallChange = 1;
+            LargeChange = 5;
+
+        }
+
+        [Category("Brush")]
+        public static readonly DependencyProperty LowColorProperty = DependencyProperty.Register(nameof(LowColor), typeof(Color), typeof(ColorPartSelector), new PropertyMetadata(Colors.Transparent));
+        public Color LowColor
+        {
+            get { return (Color)GetValue(LowColorProperty); }
+            set { SetValue(LowColorProperty, value); }
+        }
+
+        [Category("Brush")]
+        public static readonly DependencyProperty HighColorProperty = DependencyProperty.Register(nameof(HighColor), typeof(Color), typeof(ColorPartSelector), new PropertyMetadata(Colors.Black));
+        public Color HighColor
+        {
+            get { return (Color)GetValue(HighColorProperty); }
+            set { SetValue(HighColorProperty, value); }
+        }
+
+    }
+}
---
+++ LazyApiPack.Wpf.Controls/ColorSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LazyApiPack.Wpf.Controls
+{    public partial class ColorSelector : UserControl
+    {
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(nameof(SelectedColor), typeof(Color), typeof(ColorSelector), new PropertyMetadata(Colors.Black, (s, e) => ((ColorSelector)s).OnSelectedColorChanged(e.Property, (Color)e.OldValue, (Color)e.NewValue)));
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        void OnSelectedColorChanged(DependencyProperty prop, Color oldValue, Color newValue)
+        {
+            // TODO: Insert callback logic here.
+
+        }
+
+    }
+}
